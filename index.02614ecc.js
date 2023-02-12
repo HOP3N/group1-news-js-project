ef4cc57948c1795c49a197f7503d18aa7c2515ac
HOP3N <110909474+HOP3N@users.noreply.github.com>
2023-02-12 09:25:17 +0000

    Deploying to gh-pages from @ HOP3N/group1-news-js-project@1f8869682b12320dbe4451c85b809fd71272c0e7 🚀

--- index.02614ecc.js
+++ index.02614ecc.js
@@ -1,2 +1,2 @@
 var e=document.querySelectorAll("details");for(i=0;i<e.length;i++)e[i].addEventListener("toggle",t);function t(e){if(e.target.open){var t=e.target.parentNode.children;for(i=0;i<t.length;i++)"DETAILS"==t[i].tagName&&t[i].hasAttribute("open")&&e.target!=t[i]&&t[i].removeAttribute("open")}}
-//# sourceMappingURL=index.8dea34b6.js.map
+//# sourceMappingURL=index.02614ecc.js.map
--- index.9963958e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelectorAll("details");for(i=0;i<e.length;i++)e[i].addEventListener("toggle",t);function t(e){if(e.target.open){var t=e.target.parentNode.children;for(i=0;i<t.length;i++)"DETAILS"==t[i].tagName&&t[i].hasAttribute("open")&&e.target!=t[i]&&t[i].removeAttribute("open")}}}();
-//# sourceMappingURL=index.9963958e.js.map

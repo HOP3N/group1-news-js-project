320838547046e2e3838fba5c5480b39b5c680584
HOP3N <pankoff97@gmail.com>
2023-02-13 17:01:33 +0200

    import fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './js/mobile-menu';
 import './js/modal';
 import './js/pagination';
 import './js/read';
-import './js/weather';
+// import './js/weather';
 // import './js/calendar';
 // import './js/sash';

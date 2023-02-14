3af951a88072a12742f21b3cf91dfa9b54465439
CherniukAnna <chernukanna@gmail.com>
2023-02-14 16:18:12 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './js/mobile-menu';
 import './js/modal';
 import './js/pagination';
 import './js/read';
-import './js/weather';
+// import './js/weather';
 import './js/theme';
 import './js/header';
 import './js/calendar';

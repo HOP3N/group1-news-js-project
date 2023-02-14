eb654fb0b8c33fbc6b924b599f41fa4b877622aa
fdariia <fdariya3005@gmail.com>
2023-02-14 19:09:09 +0200

    Update weather.js

--- src/js/weather.js
+++ src/js/weather.js
@@ -22,7 +22,7 @@ export function getWeatherCard() {
       elem.insertAdjacentHTML('afterend', newMarkup);
     }
   });
-  // getGeoposition();
+  getGeoposition();
 }
 
 function fetchWeatherByCity(cityName) {
